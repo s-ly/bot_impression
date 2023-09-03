fa1da9a92d1a46b794b040247a328edf0b1582ba
s-ly <SergeyLysov@outlook.com>
2023-09-04 01:01:56 +0300

    добавил обработчик всех сообщений и настройки vs code

--- bot.py
+++ bot.py
@@ -7,23 +7,29 @@ import tokens
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
+
 # Объект бота
 # bot = Bot(token="12345678:AaBbCcDdEeFfGgHh")
 token_bot = tokens.token_SergeyLysovTestBot
 bot = Bot(token=token_bot)
+
 # Диспетчер
 dp = Dispatcher()
 
-# Хэндлер на команду /start
 
-
+# Отвечает на команду /start.
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
 
+
+# Отвечает на все команды, должен стоять внизу.
+@dp.message()
+async def cmd_all(message: types.Message):
+    await message.answer("All!")
+
+
 # Запуск процесса поллинга новых апдейтов
-
-
 async def main():
     await dp.start_polling(bot)
 
