d05bf19ef621be3bb4f639118de3bdb5bd1d08c0
s-ly <SergeyLysov@outlook.com>
2023-09-05 00:58:24 +0300

    учебные пробы обычных кнопок

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram import F
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from random import randint
 
 import tokens
 
@@ -22,6 +25,50 @@ dp = Dispatcher()
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
 
+
+@dp.message(Command("butt"))
+async def cmd_butt(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text="С пюрешкой")],
+        [types.KeyboardButton(text="Без пюрешки")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите способ подачи")
+    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+
+
+@dp.message(F.text.lower() == "с пюрешкой")
+async def with_puree(message: types.Message):
+    await message.reply("Отличный выбор!")
+
+
+@dp.message(F.text.lower() == "без пюрешки")
+async def without_puree(message: types.Message):
+    await message.reply("Так невкусно!")
+
+
+@dp.message(Command("random"))
+async def cmd_random(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Нажми меня",
+        callback_data="random_value")
+    )
+    await message.answer(
+        "Нажмите на кнопку, чтобы бот отправил число от 1 до 10",
+        reply_markup=builder.as_markup()
+    )
+
+@dp.callback_query(F.data == "random_value")
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer(str(randint(1, 10)))
+    await callback.answer(
+        text="Спасибо, что воспользовались ботом!",
+        show_alert=True
+    )
+    # или просто await callback.answer()
 
 # Отвечает на все команды, должен стоять внизу.
 @dp.message()
